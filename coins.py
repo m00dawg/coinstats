79235cba38056f40f96c327e273f78b9f55c976c
Tim Soderstrom <tim@moocowproductions.org>
2017-12-21 14:23:33 -0600

    Initial working solution

--- coins.py
+++ coins.py
@@ -28,19 +28,19 @@ def get_wallet(currency, address):
         print e
         pass
 
-def get_price(symbol):
+def get_currency_values():
     try:
         url = "https://poloniex.com/public?command=returnTicker";
         response = urllib.urlopen(url)
         data = json.loads(response.read())
-        return data[symbol]['last']
+        return data
     except Exception, e:
         print "Ah oh something shit the bed. Continuing Anyway"
         print e
         pass
 
 
-def put_wallet_influx(influx, currency, address, value, usd):
+def put_wallet_influx(influx, currency, address, value, usd, btc):
     try:
         json_body = [
             {
@@ -51,7 +51,8 @@ def put_wallet_influx(influx, currency, address, value, usd):
                 },
                 "fields": {
                     "value": float(value),
-                    "usd": float(usd)
+                    "usd": float(usd),
+                    "btc": float(btc)
                 }
             }
         ]
@@ -62,10 +63,18 @@ def put_wallet_influx(influx, currency, address, value, usd):
         print "Continuing Anyway"
         pass
 
+values = get_currency_values()
+
+usdt_eth = float(values['USDT_ETH']['last'])
+usdt_btc = float(values['USDT_ETH']['last'])
+usdt_ltc = float(values['USDT_ETH']['last'])
+btc_eth = float(values['BTC_LTC']['last'])
+btc_ltc = float(values['BTC_LTC']['last'])
 
 eth = weiToEth(get_wallet('eth', '0x44b4E6da63eA1e5C51715a138a17DaB612Cf329E'))
 btc = get_wallet('btc', '1FPtv6RafvzX9yjkqR2hMTSEa1GPz621X5')
 ltc = get_wallet('ltc', 'LMXdLUmr1Dt6hSbLqv1zbcku1BXtUEgifL')
-put_wallet_influx(influx, 'eth', '0x44b4E6da63eA1e5C51715a138a17DaB612Cf329E', eth, get_price('USDT_ETH'))
-put_wallet_influx(influx, 'btc', '1FPtv6RafvzX9yjkqR2hMTSEa1GPz621X5', btc, get_price('USDT_BTC'))
-put_wallet_influx(influx, 'ltc', 'LMXdLUmr1Dt6hSbLqv1zbcku1BXtUEgifL', ltc, get_price("USDT_LTC"))
+
+put_wallet_influx(influx, 'eth', '0x44b4E6da63eA1e5C51715a138a17DaB612Cf329E', eth, usdt_eth * eth, btc_eth * eth)
+put_wallet_influx(influx, 'btc', '1FPtv6RafvzX9yjkqR2hMTSEa1GPz621X5', btc, usdt_btc * btc, btc)
+put_wallet_influx(influx, 'ltc', 'LMXdLUmr1Dt6hSbLqv1zbcku1BXtUEgifL', ltc, usdt_ltc * ltc, btc_ltc * ltc)
