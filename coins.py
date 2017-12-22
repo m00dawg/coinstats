73c1f95a8387842c46e4b4a191b8f8071e50e466
Tim Soderstrom <tim@moocowproductions.org>
2017-12-21 18:01:09 -0600

    Now with more coinbase

--- coins.py
+++ coins.py
@@ -2,9 +2,14 @@
 import urllib, json, os, sys
 import ConfigParser
 from influxdb import InfluxDBClient
+from coinbase.wallet.client import Client
 
 config = ConfigParser.ConfigParser()
 config.read(os.path.dirname(sys.argv[0]) + '/config.ini')
+
+coinbase_client = Client(config.get('coinbase', 'api_key'),
+                         config.get('coinbase', 'api_secret'),
+)
 
 influx = InfluxDBClient(config.get('influx', 'host'),
                         config.get('influx', 'port'),
@@ -39,7 +44,6 @@ def get_currency_values():
         print e
         pass
 
-
 def put_wallet_influx(influx, currency, address, value, usd, btc):
     try:
         json_body = [
@@ -63,18 +67,42 @@ def put_wallet_influx(influx, currency, address, value, usd, btc):
         print "Continuing Anyway"
         pass
 
+def get_coinbase_accounts(coinbase_client):
+    coinbase_accounts = coinbase_client.get_accounts()
+    account_dict = {}
+    for account in coinbase_accounts.data:
+      currency = account.currency
+      balance = account.balance.amount
+      #print currency
+      #print balance
+      account_dict[currency] = balance
+    return account_dict
+
 values = get_currency_values()
 
 usdt_eth = float(values['USDT_ETH']['last'])
 usdt_btc = float(values['USDT_ETH']['last'])
 usdt_ltc = float(values['USDT_ETH']['last'])
+usdt_bch = float(values['USDT_BCH']['last'])
 btc_eth = float(values['BTC_LTC']['last'])
 btc_ltc = float(values['BTC_LTC']['last'])
+btc_bch = float(values['BTC_BCH']['last'])
 
+# Coinbase
+coinbase_accounts = get_coinbase_accounts(coinbase_client)
+cb_eth = float(coinbase_accounts['ETH'])
+cb_btc = float(coinbase_accounts['BTC'])
+cb_bch = float(coinbase_accounts['BCH'])
+cb_ltc  = float(coinbase_accounts['LTC'])
+put_wallet_influx(influx, 'eth', 'Coinbase', cb_eth, usdt_eth * cb_eth, btc_eth * cb_eth)
+put_wallet_influx(influx, 'btc', 'Coinbase', cb_btc, usdt_btc * cb_btc, cb_btc)
+put_wallet_influx(influx, 'bch', 'Coinbase', cb_bch, usdt_bch * cb_bch, btc_bch * cb_bch)
+put_wallet_influx(influx, 'ltc', 'Coinbase', cb_ltc, usdt_ltc * cb_ltc, btc_ltc * cb_ltc)
+
+# Cold Storage Wallets
 eth = weiToEth(get_wallet('eth', '0x44b4E6da63eA1e5C51715a138a17DaB612Cf329E'))
 btc = get_wallet('btc', '1FPtv6RafvzX9yjkqR2hMTSEa1GPz621X5')
 ltc = get_wallet('ltc', 'LMXdLUmr1Dt6hSbLqv1zbcku1BXtUEgifL')
-
 put_wallet_influx(influx, 'eth', '0x44b4E6da63eA1e5C51715a138a17DaB612Cf329E', eth, usdt_eth * eth, btc_eth * eth)
 put_wallet_influx(influx, 'btc', '1FPtv6RafvzX9yjkqR2hMTSEa1GPz621X5', btc, usdt_btc * btc, btc)
 put_wallet_influx(influx, 'ltc', 'LMXdLUmr1Dt6hSbLqv1zbcku1BXtUEgifL', ltc, usdt_ltc * ltc, btc_ltc * ltc)
